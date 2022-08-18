910ab960a1fe5904814365b066361d76c51c74af
selvarajsurendar <100891774+selvarajsurendar@users.noreply.github.com>
2022-08-18 10:21:38 -0400

    Add files via upload

---
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,38 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 17 Aug 2022 18:53:09 GMT
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
+    public static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    public static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
+    public static let colorBaseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    public static let colorBaseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    public static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    public static let globalDjTypographyHeadlineFontSizeL04col = 22px
+    public static let globalDjTypographyHeadlineFontSizeL08col = 22px
+    public static let globalDjTypographyHeadlineFontSizeL12col = 32
+    public static let globalDjTypographyHeadlineFontSizeL16col = 33px
+    public static let globalDjTypographyHeadlineFontSizeM04col = 22px
+    public static let globalDjTypographyHeadlineFontSizeM08col = 22px
+    public static let globalDjTypographyHeadlineFontSizeM12col = 32
+    public static let globalDjTypographyHeadlineFontSizeM16col = 33px
+    public static let globalDjTypographyHeadlineFontSizeS04col = 16px
+    public static let globalDjTypographyHeadlineFontSizeS08col = 18px
+    public static let globalDjTypographyHeadlineFontSizeS12col = 22px
+    public static let globalDjTypographyHeadlineFontSizeS16col = 22px
+    public static let globalDjTypographyHeadlineFontSizeXl04col = 22
+    public static let globalDjTypographyHeadlineFontSizeXl08col = 28
+    public static let globalDjTypographyHeadlineFontSizeXl12col = 36
+    public static let globalDjTypographyHeadlineFontSizeXl16col = 40
+    public static let globalFontRem = 1rem
+}
---
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,38 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 17 Aug 2022 18:53:09 GMT
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
+    internal static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    internal static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
+    internal static let colorBaseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    internal static let colorBaseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    internal static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    internal static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    internal static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    internal static let globalDjTypographyHeadlineFontSizeL04col = 22px
+    internal static let globalDjTypographyHeadlineFontSizeL08col = 22px
+    internal static let globalDjTypographyHeadlineFontSizeL12col = 32
+    internal static let globalDjTypographyHeadlineFontSizeL16col = 33px
+    internal static let globalDjTypographyHeadlineFontSizeM04col = 22px
+    internal static let globalDjTypographyHeadlineFontSizeM08col = 22px
+    internal static let globalDjTypographyHeadlineFontSizeM12col = 32
+    internal static let globalDjTypographyHeadlineFontSizeM16col = 33px
+    internal static let globalDjTypographyHeadlineFontSizeS04col = 16px
+    internal static let globalDjTypographyHeadlineFontSizeS08col = 18px
+    internal static let globalDjTypographyHeadlineFontSizeS12col = 22px
+    internal static let globalDjTypographyHeadlineFontSizeS16col = 22px
+    internal static let globalDjTypographyHeadlineFontSizeXl04col = 22
+    internal static let globalDjTypographyHeadlineFontSizeXl08col = 28
+    internal static let globalDjTypographyHeadlineFontSizeXl12col = 36
+    internal static let globalDjTypographyHeadlineFontSizeXl16col = 40
+    internal static let globalFontRem = 1rem
+}
